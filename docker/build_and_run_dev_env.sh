ebd0200367f992cf6074aa25552fe36f06293ed3
Kaspars Mickevics <kaspars@fx.lv>
2020-03-11 17:00:33 +0200

    docker envs for testing and development

---
+++ docker/build_and_run_dev_env.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile.sscom-tracker -t sscom-tracker --no-cache ../
+docker run -ti sscom-tracker bash
---
+++ docker/build_base_image.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile.sscom-tracker-base -t sscom-tracker-base --no-cache ../
