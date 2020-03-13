6aa5f376c5d297a33f28fbd18bb61ae6b73e508f
Kaspars Mickevics <kaspars@fx.lv>
2020-03-13 19:14:11 +0200

    lint code in lib/*

--- testing.sh
+++ testing.sh
@@ -2,16 +2,21 @@
 echo
 echo
 echo "Running Pylint"
+pylint lib/*.py
 pylint *.py
 echo
 echo "Running Pydocstyle"
+pydocstyle -v lib/*.py
 pydocstyle -v *.py
 echo
 echo "Running Prospector"
+prospector lib/*.py
 prospector *.py
 echo "Cyclomatic Complexity"
+radon cc lib/*.py
 radon cc *.py
 echo "maintainability index"
+radon mi lib/*.py
 radon mi *.py
 echo "Running tests"
 echo
