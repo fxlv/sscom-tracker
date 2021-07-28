4c367f04b6af28c39d72e26a3fc9b49c65957177
Kaspars Mickevics <kaspars@fx.lv>
2021-07-28 20:40:53 +0300

    developing rss retriever

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -3,9 +3,18 @@ import lib.cache
 import lib.retriever
 import lib.settings
 import feedparser
+import os
 
 
 class TestRetriever:
+    @pytest.fixture(scope="class")
+    def chdir(self, request):
+        print(f"dirname: {request.fspath.dirname}")
+        print(f"invocation dir: {request.config.invocation_dir}")
+        os.chdir(request.fspath.dirname)
+        yield
+        os.chdir(request.config.invocation_dir)
+
     def test_basic_retrieval(self):
         r = lib.retriever.RSSRetriever()
         result: feedparser.util.FeedParserDict = r.get(
@@ -15,7 +24,7 @@ class TestRetriever:
         assert result.status == 200
         assert isinstance(result.entries, list)
 
-    def test_retrieval_with_cache(self):
+    def test_retrieval_with_cache(self, chdir):
         settings = lib.settings.Settings("settings.test.json")
         settings.data_cache = "retrieval_cache.db"
         cache = lib.cache.DataCache(settings)
