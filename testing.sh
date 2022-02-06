51f1e989c79e962c5a9a12279f9ea9e7f0e53a7f
Kaspars Mickevics <kaspars@fx.lv>
2022-02-06 18:49:57 +0200

    support running only tests or all

--- testing.sh
+++ testing.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
-echo
-echo
-echo "Running Pylint"
-pylint lib/*.py
-pylint *.py
-echo
-echo "Running Pydocstyle"
-pydocstyle -v lib/*.py
-pydocstyle -v *.py
-echo
-echo "Running Prospector"
-prospector lib/*.py
-prospector *.py
-echo "Cyclomatic Complexity"
-radon cc lib/*.py
-radon cc *.py
-echo
-echo "maintainability index"
-radon mi lib/*.py
-radon mi *.py
-echo
-echo "Running mypy"
-python3 -m mypy tracker.py
-echo
 
-echo "Running pre-commit"
-pre-commit run  --all-files
-echo
-echo "Running tests"
-echo
-pytest -v --cov-report term-missing --cov='lib/' --cov='./tracker.py' -v
-echo "All done."
+if [[ -z $1 ]]; then
+    echo "Please provide an argument"
+    echo "Supported arguments:"
+    echo "  - all"
+    echo "  - test"
+    exit 0
+fi
+
+
+if [[ $1 == "all" ]];then
+    echo
+    echo
+    echo "Running Pylint"
+    pylint lib/*.py
+    pylint *.py
+    echo
+    echo "Running Pydocstyle"
+    pydocstyle -v lib/*.py
+    pydocstyle -v *.py
+    echo
+    echo "Running Prospector"
+    prospector lib/*.py
+    prospector *.py
+    echo "Cyclomatic Complexity"
+    radon cc lib/*.py
+    radon cc *.py
+    echo
+    echo "maintainability index"
+    radon mi lib/*.py
+    radon mi *.py
+    echo
+    echo "Running mypy"
+    python3 -m mypy tracker.py
+    echo
+
+    echo "Running pre-commit"
+    pre-commit run  --all-files
+    echo
+    echo "Running tests"
+    echo
+    pytest -v --cov-report term-missing --cov='lib/' --cov='./tracker.py' -v
+    echo "All done."
+elif [[ $1 == "test" ]]; then
+    pytest -v --cov-report term-missing --cov='lib/' --cov='./tracker.py' -v
+else
+    echo "Unsupported argument"
+fi
+
