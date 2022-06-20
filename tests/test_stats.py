4b38e402a8d47409aa1e73ef6997542eeee3d154
Kaspars Mickevics <kaspars@fx.lv>
2022-06-20 15:36:30 +0300

    delete the pickle based stats class and its tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -8,12 +8,8 @@ import lib.stats
 
 RSS_FILES_COUNT = 5
 
-def stats_pickle():
-    settings = lib.settings.Settings("settings.test.json")
-    stats = lib.stats.TrackerStatsPickle(settings)
-    return stats
-
-def stats_sql():
+@pytest.fixture
+def stats():
     settings = lib.settings.Settings("settings.test.json")
     stats = lib.stats.TrackerStatsSql(settings)
     return stats
@@ -31,7 +27,6 @@ def test_stats_data():
     assert data.http_data == (None, None)
     assert data.enrichment_data == (None, None)
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_get_last_rss_update(stats):
     now = arrow.now()
     stats.set_last_rss_update(now.datetime)
@@ -39,39 +34,34 @@ def test_set_get_last_rss_update(stats):
 
 
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_get_rss_files_count(stats):
     stats.set_rss_files_count(RSS_FILES_COUNT)
     assert stats.get_rss_files_count() == RSS_FILES_COUNT
 
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_get_objects_files_count(stats):
     stats.set_objects_files_count("test", 7)
     assert stats.get_objects_files_count("test") == 7
 
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_get_http_data_stats(stats):
     stats.set_http_data_stats(1, 1)
     assert stats.get_http_data_stats() == (1, 1)
 
 
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
+#@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_get_enrichment_stats(stats):
     stats.set_enrichment_stats(1, 1)
     assert stats.get_enrichment_stats() == (1, 1)
 
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_set_last_objects_update(stats):
     stats.set_last_objects_update()
     now = arrow.now()
     delta = now - stats.get_last_objects_update()
     assert delta.total_seconds() < 1
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_get_last_objects_update(stats):
     stats.set_last_objects_update()
     last = stats.get_last_objects_update()
@@ -79,7 +69,6 @@ def test_get_last_objects_update(stats):
     delta = now - last
     assert delta.total_seconds() < 1
 
-@pytest.mark.parametrize("stats", [stats_pickle(), stats_sql()], ids=["pickle", "sql"])
 def test_last_objects_update_throttling_works(stats):
     """Last objects update is throttled to once per minute.
     
