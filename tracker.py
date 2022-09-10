67bccb583d57d858f15ab25e50b7d84f75c06cee
Kaspars Mickevics <kaspars@fx.lv>
2022-09-10 14:42:08 +0300

    refactoring

--- tracker.py
+++ tracker.py
@@ -6,11 +6,9 @@
 #
 # kaspars@fx.lv
 #
-import json
 import random
 import sys
 import time
-from os import CLD_CONTINUED
 
 import click
 from loguru import logger
@@ -23,12 +21,9 @@ import lib.push
 import lib.retriever
 import lib.rssstore
 import lib.settings
-from lib.display import print_results_to_console
-from lib.filter import Filter
-from lib.log import func_log, normalize, set_up_logging
-from lib.push import send_push
 import lib.stats
-
+from lib.display import print_results_to_console
+from lib.log import func_log, set_up_logging
 from lib.zabbix import Zabbix, ZabbixStopwatch
 
 
@@ -50,17 +45,16 @@ def update(debug, category):
     set_up_logging(settings, debug)
     with logger.contextualize(task="Update"):
         z = Zabbix(settings)
-        update_stopwatch = ZabbixStopwatch(z,"update_time_seconds")
+        update_stopwatch = ZabbixStopwatch(z, "update_time_seconds")
         logger.info(f"Updating '{category}'...")
         rm = lib.retriever.RetrieverManager(settings)
         rm.update_all(category)
-        del(rm) # destructors are unreliable
+        del rm  # destructors are unreliable
         logger.info("Updating run complete")
         update_stopwatch.done()
 
 
-
-#def object_processor(rss_object_queue,settings,z):
+# def object_processor(rss_object_queue,settings,z):
 def object_processor(work_queue):
     if work_queue is False:
         return True
@@ -74,14 +68,14 @@ def object_processor(work_queue):
         logger.trace("Trying to get object from the queue")
         object_stopwatch = ZabbixStopwatch(z, "rss_object_processing")
         stats = lib.stats.TrackerStatsSql(settings)
-        rss_object_hash = lib.helpers.hash(str(rss_object.url_hash)+str(rss_object.updated))
+        rss_object_hash = lib.helpers.hash(str(rss_object.url_hash) + str(rss_object.updated))
         if stats.check_if_rss_file_was_parsed(rss_object_hash):
             logger.debug("This RSS object has been parsed already, skipping")
             return True
         logger.debug("Parsing a new RSS object")
         parsed_list = op.parse_object(rss_object)
         logger.trace(f"Object parsed in {object_stopwatch.get()} seconds")
-        #object_parsing_stats.append(object_stopwatch.get())
+        # object_parsing_stats.append(object_stopwatch.get())
         logger.debug(
             f"[{rss_object.url_hash[:10]}] RSS object parsed, now writing/updating classifieds"
         )
@@ -94,7 +88,8 @@ def object_processor(work_queue):
                 object_store.write_classified(classified)
         logger.trace(f"Object processing complete in {object_stopwatch.get()} seconds")
         stats.mark_rss_file_as_parsed(rss_object_hash)
-        #object_processing_stats.append(object_stopwatch.get())
+        # object_processing_stats.append(object_stopwatch.get())
+
 
 @func_log
 @cli.command()
@@ -107,31 +102,26 @@ def process(debug, all):
 
     with logger.contextualize(task="Process"):
         z = Zabbix(settings)
-        process_stopwatch = ZabbixStopwatch(z,"processing_time_seconds")
+        process_stopwatch = ZabbixStopwatch(z, "processing_time_seconds")
 
         logger.info(f"Starting processing run...{all=}")
         store = lib.rssstore.RSSStore(settings)
-        op = lib.objectparser.ObjectParser()
-        object_store = lib.objectstore.ObjectStoreSqlite(settings)
         objects_list = store.load(all)
-        object_parsing_stats = []
-        object_processing_stats = []
-        from multiprocessing import Pool,Queue, Process
+        from multiprocessing import Pool
         q = []
         for rss_object in objects_list:
             q.append((rss_object, settings))
-        q.append(False) # signaling end of queue
+        q.append(False)  # signaling end of queue
         p = Pool(2)
-        p.map(object_processor,q)
+        p.map(object_processor, q)
 
-        #object_processor(rss_object,object_store,op,z)
+        # object_processor(rss_object,object_store,op,z)
 
         logger.info("Processing run complete")
-        #z.send_int_to_zabbix("object_processing_time_min", min(object_processing_stats))
-        #z.send_int_to_zabbix("object_parsing_time_min", min(object_parsing_stats))
-        #z.send_int_to_zabbix("object_processing_time_max", max(object_processing_stats))
-        #z.send_int_to_zabbix("object_parsing_time_max", max(object_parsing_stats))
-        del object_store  # exlplicitly deleting object calls its destructor and we are making sure to do that while still within the logging context
+        # z.send_int_to_zabbix("object_processing_time_min", min(object_processing_stats))
+        # z.send_int_to_zabbix("object_parsing_time_min", min(object_parsing_stats))
+        # z.send_int_to_zabbix("object_processing_time_max", max(object_processing_stats))
+        # z.send_int_to_zabbix("object_parsing_time_max", max(object_parsing_stats))
         del store
         process_stopwatch.done()
 
@@ -173,7 +163,7 @@ def enrich(debug, force):
     set_up_logging(settings, debug)
     with logger.contextualize(task="Enricher"):
         z = Zabbix(settings)
-        enrich_stopwatch = ZabbixStopwatch(z,"enrichment_time_seconds")
+        enrich_stopwatch = ZabbixStopwatch(z, "enrichment_time_seconds")
         logger.debug("Running enricher...")
         object_store = lib.objectstore.ObjectStoreSqlite(settings)
         enricher = lib.objectparser.Enricher()
@@ -233,7 +223,8 @@ def stats(debug):
 @func_log
 @cli.command()
 @click.option("--debug", is_flag=True, default=False, help="Print DEBUG log to screen")
-@click.option("--force", is_flag=True, default=False, help="Force the retrieval, even if it was already previously retrieved")
+@click.option("--force", is_flag=True, default=False,
+              help="Force the retrieval, even if it was already previously retrieved")
 def retr(debug, force):
     """Retrieve the per-classified details using HTTP."""
     settings = lib.settings.Settings()
@@ -241,7 +232,7 @@ def retr(debug, force):
     with logger.contextualize(task="URL retriever"):
         logger.debug("Running retriever...")
         z = Zabbix(settings)
-        retr_stopwatch = ZabbixStopwatch(z,"retrieval_time_seconds")
+        retr_stopwatch = ZabbixStopwatch(z, "retrieval_time_seconds")
         object_store = lib.objectstore.ObjectStoreSqlite(settings)
         hr = lib.retriever.HttpRetriever()
         # iterate over all objects
@@ -250,13 +241,12 @@ def retr(debug, force):
 
         # get all classifieds that do not have http_response_data
 
-
         for classified in object_store.get_all_classifieds("*", for_retrieval=True):
             if hasattr(classified, "http_response_data"):
                 logger.trace(
                     f"Object {classified.short_hash} already has http_response_data"
                 )
-                if classified.http_response_data != None and not force:
+                if classified.http_response_data is not None and not force:
                     logger.trace(
                         f"Object {classified.short_hash} already contains http response data, skipping"
                     )
@@ -269,7 +259,8 @@ def retr(debug, force):
                     classified.http_response_data = http_response.response_content
                     classified.http_response_code = http_response.response_code
                     if classified.category == "apartment":
-                        classified.coordinates_string = hr.retrieve_coordinates_from_raw_http_data(http_response.response_raw)
+                        classified.coordinates_string = hr.retrieve_coordinates_from_raw_http_data(
+                            http_response.response_raw)
                     object_store.update_classified(classified)
                     # randomsleep()
             else:
